e3b4b60c56c99271cee4b72f7c37cf273a783e36
yael <dote.dg.p@gmail.com>
2025-06-26 19:20:51 +0300

    connecting & starting with csv

--- comedy file/comedy.js
+++ comedy file/comedy.js
@@ -1,9 +1,24 @@
 async function movies(){
     const response = await fetch("../../movies.csv");
 
-    const data = await response.text
+    const data = await response.text();
     
+
+    Papa.parse(data, {
+        complete: function(results) {
+         console.log(results.data);
+
+         const bananasImg = document.getElementById("img1");
+         bananasImg.src = results.data[1][1];
+         const bananasInfo = document.getElementById("p1");
+         bananasInfo.src = results.data[1][2];
+
+
+    }});
+
     console.log(data);
+
+
 }
 
 movies();
