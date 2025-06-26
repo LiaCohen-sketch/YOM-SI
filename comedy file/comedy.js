a3f68124c022cb88d4cfba3f7e23e7c246834e95
yael <dote.dg.p@gmail.com>
2025-06-26 19:36:58 +0300

    three movies are done!

--- comedy file/comedy.js
+++ comedy file/comedy.js
@@ -15,6 +15,20 @@ async function movies(){
          const bananasName = document.getElementById("s1");
          bananasName.textContent = results.data[1][0];
 
+         const TMTR_img = document.getElementById("img2");
+         TMTR_img.src = results.data[2][1];
+         const TMTR_info = document.getElementById("p2");
+         TMTR_info.textContent = results.data[2][2];
+         const TMTR_name = document.getElementById("s2");
+         TMTR_name.textContent = results.data[2][0];
+
+         const TGR_img = document.getElementById("img3");
+         TGR_img.src = results.data[3][1];
+         const TGR_info = document.getElementById("p3");
+         TGR_info.textContent = results.data[3][2];
+         const TGR_name = document.getElementById("s3");
+         TGR_name.textContent = results.data[3][0];
+
 
     }});
 
