481178832190a3812e4ee5d68f44de18b66cf87f
yael <dote.dg.p@gmail.com>
2025-06-27 18:25:58 +0300

    first 3 romance movie finish

--- romance file/romance.js
+++ romance file/romance.js
@@ -15,6 +15,20 @@
             const casablanca_Name = document.getElementById("s1");
             casablanca_Name.textContent = results.data[9][0];
 
+            const TSATC_Img = document.getElementById("img2");
+            TSATC_Img.src = results.data[10][1];
+            const TSATC_Info = document.getElementById("p2");
+            TSATC_Info.textContent = results.data[10][2];
+            const TSATC_Name = document.getElementById("s2");
+            TSATC_Name.textContent = results.data[10][0];
+
+            const ah_Img = document.getElementById("img3");
+            ah_Img.src = results.data[11][1];
+            const ah_Info = document.getElementById("p3");
+            ah_Info.textContent = results.data[11][2];
+            const ah_Name = document.getElementById("s3");
+            ah_Name.textContent = results.data[11][0];
+
         }});
 
         console.log(data);
