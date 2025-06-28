1675ecd32566a4399f698f1cedb4f76ca3374c0c
yael <dote.dg.p@gmail.com>
2025-06-28 15:27:11 +0300

    3 animated movies left

--- animation file/animation.js
+++ animation file/animation.js
@@ -22,26 +22,26 @@ async function movies(){
          const TA_name = document.getElementById("s2");
          TA_name.textContent = results.data[18][0];
 
-         const TGR_img = document.getElementById("img3");
-         TGR_img.src = results.data[3][1];
-         const TGR_info = document.getElementById("p3");
-         TGR_info.textContent = results.data[3][2];
-         const TGR_name = document.getElementById("s3");
-         TGR_name.textContent = results.data[3][0];
+         const RH_img = document.getElementById("img3");
+         RH_img.src = results.data[19][1];
+         const RH_info = document.getElementById("p3");
+         RH_info.textContent = results.data[19][2];
+         const RH_name = document.getElementById("s3");
+         RH_name.textContent = results.data[19][0];
 
-         const MT_img = document.getElementById("img4");
-         MT_img.src = results.data[4][1];
-         const MT_info = document.getElementById("p4");
-         MT_info.textContent = results.data[4][2];
-         const MT_name = document.getElementById("s4");
-         MT_name.textContent = results.data[4][0];
+         const TMAOWTP_img = document.getElementById("img4");
+         TMAOWTP_img.src = results.data[20][1];
+         const TMAOWTP_info = document.getElementById("p4");
+         TMAOWTP_info.textContent = results.data[20][2];
+         const TMAOWTP_name = document.getElementById("s4");
+         TMAOWTP_name.textContent = results.data[20][0];
 
-         const TGD_img = document.getElementById("img5");
-         TGD_img.src = results.data[5][1];
-         const TGD_info = document.getElementById("p5");
-         TGD_info.textContent = results.data[5][2];
-         const TGD_name = document.getElementById("s5");
-         TGD_name.textContent = results.data[5][0];
+         const pinocchio_img = document.getElementById("img5");
+         pinocchio_img.src = results.data[21][1];
+         const pinocchio_info = document.getElementById("p5");
+         pinocchio_info.textContent = results.data[21][2];
+         const pinocchio_name = document.getElementById("s5");
+         pinocchio_name.textContent = results.data[21][0];
 
          const opera_img = document.getElementById("img6");
          opera_img.src = results.data[6][1];
