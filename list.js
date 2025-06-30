03050111de66ac3b3a520a95903c5f6475db774f
yael <dote.dg.p@gmail.com>
2025-06-30 20:16:09 +0300

    all animation's list button done

--- list.js
+++ list.js
@@ -1,13 +1,16 @@
-let fav_list = document.getElementById("list");
-fav_list.innerHTML = `
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap');
-    </style>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&display=swap');
-    </style>
-    <link rel="stylesheet" href="../list.css">
-    <div class="add">
-      <button id="add" onclick="add()">+</button><button id="ad">Add To favorite</button>    
-    </div>
-  `;
+document.querySelectorAll(".list").forEach(fav_list => {
+    fav_list.innerHTML = `
+      <div class="add">
+        <button id="add" onclick="add()">+</button>
+        <button id="ad">Add To favorite</button>
+      </div>
+    `;
+  });
+
+  document.getElementsByClassName("list").innerHTML = `
+        <div class="add">
+        <button id="add" onclick="add()">+</button>
+        <button id="ad">Add To favorite</button>
+      </div>
+  `
+  
